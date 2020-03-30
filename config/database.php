f0d8a30ce43c77747b8a64f63ebf5b148811e808
masenu95 <masenu95@gmail.com>
2020-03-31 02:19:33 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -66,7 +66,13 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'read'     => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+            ],
+            'write'     => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+            ],
+
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
