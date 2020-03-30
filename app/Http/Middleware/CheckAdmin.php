c58e5507d7dd53482563a4311495289032c05865
masenu95 <masenu95@gmail.com>
2020-03-31 01:52:06 +0300

    Update CheckAdmin.php

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -16,7 +16,7 @@ class CheckAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(!Auth::check() || Auth::user()->role !== 1){
+        if( Auth::user()->role != 1){
                 return redirect('Page');
         }
         return $next($request);
