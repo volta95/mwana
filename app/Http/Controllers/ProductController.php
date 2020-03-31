56e86a30814e4524079ec4d79b749f9c72d1f562
masenu95 <masenu95@gmail.com>
2020-03-31 03:46:39 +0300

    change

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,6 +17,13 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index','show',]);
+
+    }
+
     public function index()
     {
         //
